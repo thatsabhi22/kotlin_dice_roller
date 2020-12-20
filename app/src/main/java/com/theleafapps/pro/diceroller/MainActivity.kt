05102e5a5c54cf06b81534a342c840e2826c7bf6
The Aviator <redefinedabhi2@gmail.com>
2020-12-20 20:41:18 +0530

    Efficiently initialising ImageView and Button with lateInit

--- app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dice_iv:ImageView
+    lateinit var roll_btn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val roll_btn: Button = findViewById(R.id.roll_btn)
+        roll_btn = findViewById(R.id.roll_btn)
+        dice_iv = findViewById(R.id.dice_iv)
+
         roll_btn.text = "Let's Roll"
         roll_btn.setOnClickListener {
             rollDice()
@@ -19,9 +25,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice_iv: ImageView = findViewById(R.id.dice_iv)
-
-
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
