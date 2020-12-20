27eb0a10bad8acff06ad493ec17c85cdcc56e6c4
The Aviator <redefinedabhi2@gmail.com>
2020-12-20 20:32:06 +0530

    Current Status: App Almost Complete Added Dice Images for all faces Dice button function working fine

--- app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.theleafapps.pro.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +12,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val roll_btn: Button = findViewById(R.id.roll_btn)
-        roll_btn.text="Let's Roll"
-        roll_btn.setOnClickListener{
-            Toast.makeText(this,"Rolling ....",Toast.LENGTH_LONG).show()
+        roll_btn.text = "Let's Roll"
+        roll_btn.setOnClickListener {
+            rollDice()
         }
+    }
 
+    private fun rollDice() {
+        val dice_iv: ImageView = findViewById(R.id.dice_iv)
+
+
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_iv.setImageResource(drawableResource)
     }
 }
