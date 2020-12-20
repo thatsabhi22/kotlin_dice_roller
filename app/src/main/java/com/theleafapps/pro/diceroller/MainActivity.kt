3f52d6a264cfd699e654fa27172a8bce742d823e
The Aviator <redefinedabhi2@gmail.com>
2020-12-20 20:05:14 +0530

    Roller Button set with OnClickListener

--- app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/diceroller/MainActivity.kt
@@ -2,10 +2,19 @@ package com.theleafapps.pro.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val roll_btn: Button = findViewById(R.id.roll_btn)
+        roll_btn.text="Let's Roll"
+        roll_btn.setOnClickListener{
+            Toast.makeText(this,"Rolling ....",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
